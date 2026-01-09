34a83900941fc26169cb9937df8f666e0f81b334
jchukkarin <jchukkarin@gmail.com>
2026-01-09 21:39:38 +0700

    edit

---
+++ app/edit/page.tsx
@@ -0,0 +1,7 @@
+export default function EditPage() {
+    return (
+        <div className="min-h-screen bg-[#F8F9FC] font-sans text-gray-900">
+            <h1>Setting</h1>
+        </div>
+    );
+}
